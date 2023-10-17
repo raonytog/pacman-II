85cafcb1319ae15a68b741ef19bddd21fc8d92eb
raonytog <raony.gomes@edu.ufes.br>
2023-10-17 10:55:11 -0300

    fazendo mapa

--- Respostas/Raony/tMapa.c
+++ Respostas/Raony/tMapa.c
@@ -2,6 +2,7 @@
 
 #define COMIDA '*'
 #define PORTAL '@'
+#define PAREDE '#'
 
 /**
  * Cria o mapa dinamicamente
@@ -17,23 +18,24 @@ tMapa* CriaMapa(const char* caminhoConfig) {
         exit(1);
     }
 
+    // a implementar
+
     return mapa;
 }
 
-/**
- * Obtem a posição de um item do mapa
- * \param mapa mapa
- * \param item item a ser procurado no mapa
- */
 tPosicao* ObtemPosicaoItemMapa(tMapa* mapa, char item) {
-
+     for (int i = 0; i < mapa->nLinhas; i++)
+        for (int j = 0; j < mapa->nColunas; j++)
+            if (mapa->grid[i][j] == item) {
+                tPosicao * posicao = CriaPosicao(i, j);
+                return posicao;
+            }
 }
 
-/**
- * Retorna o túnel do mapa com os 2 acessos
- * \param mapa mapa
- */
-tTunel* ObtemTunelMapa(tMapa* mapa);
+tTunel* ObtemTunelMapa(tMapa* mapa) {
+    tTunel * tunel = mapa->tunel;
+    return tunel;
+}
 
 char ObtemItemMapa(tMapa* mapa, tPosicao* posicao) {
     return mapa->grid[posicao->linha][posicao->coluna];
@@ -61,27 +63,17 @@ int ObtemNumeroMaximoMovimentosMapa(tMapa* mapa) {
     return mapa->nMaximoMovimentos;
 }
 
-/**
- * Retorna se a posição passada como parâmetro representa uma comida no mapa
- * \param mapa mapa
- * \param posicao posicao a ser verificada
- */
-bool EncontrouComidaMapa(tMapa* mapa, tPosicao* posicao);
+bool EncontrouComidaMapa(tMapa* mapa, tPosicao* posicao) {
+    return mapa->grid[posicao->linha][posicao->coluna] == COMIDA;
+}
 
-/**
- * Retorna se a posição passada como parâmetro representa uma parede no mapa
- * \param mapa mapa
- * \param posicao posicao a ser verificada
- */
-bool EncontrouParedeMapa(tMapa* mapa, tPosicao* posicao);
+bool EncontrouParedeMapa(tMapa* mapa, tPosicao* posicao) {
+    return mapa->grid[posicao->linha][posicao->coluna] == PAREDE;
+}
 
-/**
- * Atualiza um item do mapa
- * \param mapa mapa
- * \param posicao posicao do item
- * \param item posicao item que vai atualizar o mapa
- */
-bool AtualizaItemMapa(tMapa* mapa, tPosicao* posicao, char item);
+void AtualizaItemMapa(tMapa* mapa, tPosicao* posicao, char item) {
+    mapa->grid[posicao->linha][posicao->coluna] = item;
+}
 
 bool PossuiTunelMapa(tMapa* mapa) {
     for (int i = 0; i < mapa->nLinhas; i++)
@@ -98,8 +90,7 @@ void EntraTunelMapa(tMapa* mapa, tPosicao* posicao) {
     LevaFinalTunel(mapa->tunel, posicao);
 }
 
-/**
- * Libera o espaço alocado para a estrutura tMapa
- * \param mapa mapa
- */
-void DesalocaMapa(tMapa* mapa);
+void DesalocaMapa(tMapa* mapa) {
+    free(mapa->tunel);
+    free(mapa);
+}
